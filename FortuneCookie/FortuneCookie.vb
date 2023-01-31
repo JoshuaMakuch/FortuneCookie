5dcc09da2cbe31435ddc369f33d2b4e1fd77fa7e
JoshuaMakuch <joshuamakuch9@gmail.com>
2023-01-31 08:14:27 -0700

    Add project files.

---
+++ FortuneCookie/FortuneCookie.vb
@@ -0,0 +1,15 @@
+'Joshua Makuch
+'RCET 0265
+'Fall 2023
+'
+
+Option Explicit On
+Option Strict On
+
+Imports System
+
+Module FortuneCookie
+    Sub Main(args As String())
+        Console.WriteLine("Hello World!")
+    End Sub
+End Module
