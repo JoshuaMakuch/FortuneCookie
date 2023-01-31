ea5fd5cc16acfcfb427c23a7aca169958d02d414
JoshuaMakuch <joshuamakuch9@gmail.com>
2023-01-31 09:03:40 -0700

    Got the random number generator working that outputs however many wisdoms the user wants.

--- FortuneCookie/FortuneCookie.vb
+++ FortuneCookie/FortuneCookie.vb
@@ -1,7 +1,8 @@
 'Joshua Makuch
 'RCET 0265
 'Fall 2023
-'
+'Fortune Cookie
+'https://github.com/JoshuaMakuch/FortuneCookie.git
 
 Option Explicit On
 Option Strict On
@@ -10,6 +11,50 @@ Imports System
 
 Module FortuneCookie
     Sub Main(args As String())
-        Console.WriteLine("Hello World!")
+
+        Randomize()
+
+        Dim randomNumber As Integer
+        Dim userInput As Integer
+
+        'Asks the user how many fortune cookies the want
+        Do
+            Console.WriteLine("")
+            Console.WriteLine("How many fortune cookies would you like")
+            Try
+                userInput = CInt(Console.ReadLine())
+                Console.WriteLine("")
+                Console.WriteLine($"You chose {userInput} fortune cookies.")
+                If userInput < 1001 And userInput >= 0 Then
+                    Exit Do
+                Else
+                    Console.WriteLine("")
+                    Console.WriteLine("That won't work, its a bit ludicrous of you to think it would >:(.")
+                End If
+            Catch ex As Exception
+                Console.WriteLine("")
+                Console.WriteLine("That amount is NO WORKY!")
+            End Try
+        Loop
+
+        'This loops however many times the user input
+        For i = 1 To userInput
+            'This takes a random fraction and multiplies it by 2 then turns it into an integer, including rounding.
+            'This creates a number from 0 to 2, one of three possible options
+            randomNumber = CInt(Rnd() * 2)
+
+            Select Case randomNumber
+                Case 0
+                    Console.WriteLine("You obtained Wisdom 1: OJSDJGOSNGFODSFNGODFSJGNUDF")
+                Case 1
+                    Console.WriteLine("You obtained Wisdom 2: YOU DO NOT RECOGNIZE THE COGNITO HAZARD")
+                Case 2
+                    Console.WriteLine("You obtained Wisdom 3: THERE ARE SEVERAL KETER CLASS CONTAINMENT BREACHES IN THE FACILITY")
+            End Select
+        Next
+
+
+
+
     End Sub
 End Module
